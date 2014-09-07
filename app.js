aa6693b9394b27d62f7804b2adeacfad2a8a332c
Audiosurfer <gabolad@hotmail.com>
2014-09-07 15:36:09 -0500

    now takes user input
    takes user input to decide what number to count to, and it has the user
    reselect in the event a number isn't inputted or if the number has
    decimals.

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
-    for (i=1; i <= 100; i++) {
-       if (i % 3 === 0 && i % 5 === 0) {
-            $('body').append("<p>FizzBuzz</p>");
-              }
-        else if (i % 3 === 0) {
-              $('body').append("<p>Fizz</p>");
-                }
-        else if (i % 5 === 0) {
-             $('body').append("<p>Buzz</p>");
-               }
-        else {
-               $('body').append("<p>" + i + "</p>");
-               }   
+    var fizzBuzz = function() {
+       var number = prompt("What number should we count to?"); 
+       number = +number;
+       while (isNaN(number)) {
+            number = prompt("Please input a number");
+        }
+       while (number % 1 != 0) {
+           number = prompt("Please choose a whole number");
+       }
+        for (i=1; i <= number; i++) {
+           if (i % 3 === 0 && i % 5 === 0) {
+                $('body').append("<p>FizzBuzz</p>");
+                  }
+            else if (i % 3 === 0) {
+                  $('body').append("<p>Fizz</p>");
+                    }
+            else if (i % 5 === 0) {
+                 $('body').append("<p>Buzz</p>");
+                   }
+            else {
+                   $('body').append("<p>" + i + "</p>");
+                   }   
+        }
     }
+    fizzBuzz();
 });
